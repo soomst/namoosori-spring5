1e48f62769fccc271e94b8ba5a63e6aab49e0d1b
resoom <dogok1703@gmail.com>
2022-11-30 00:22:38 +0900

    put, delete mapping

--- travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/aggregate/club/TravelClub.java
+++ travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/aggregate/club/TravelClub.java
@@ -94,5 +94,11 @@ public class TravelClub extends Entity {
 	public static void main(String[] args) {
 		//
 		System.out.println(new Gson().toJson(sample()));
+
+		NameValueList list = new NameValueList();
+		list.addNameValue("name", "soomsoom");
+		list.addNameValue("intro", "Change Club Intro!!");
+
+		System.out.println(new Gson().toJson(list));
 	}
 }
--- travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
+++ travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
@@ -2,15 +2,19 @@ package io.namoosori.travelclub.web.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.namoosori.travelclub.web.aggregate.club.TravelClub;
 import io.namoosori.travelclub.web.service.ClubService;
 import io.namoosori.travelclub.web.service.sdo.TravelClubCdo;
+import io.namoosori.travelclub.web.shared.NameValueList;
 
 @RestController
 public class ClubController {
@@ -31,9 +35,24 @@ public class ClubController {
         return clubService.findAll();
     }
 
-    @GetMapping("/club/{clubId}")
+    @GetMapping("/club/{clubId}") //localhost:8090/club/clubId값
     public TravelClub find(@PathVariable String clubId) {
         return clubService.findClubById(clubId);
+    }
+
+    @GetMapping("/club")  //localhost:8090/club?name=JavaClub
+    public List<TravelClub> findByName(@RequestParam String name) {
+        return clubService.findClubsByName(name);
+    }
+
+    @PutMapping("/club/{clubId}")
+    public void modify(@PathVariable String clubId, @RequestBody NameValueList nameValueList) {
+        clubService.modify(clubId, nameValueList);
+    }
+
+    @DeleteMapping("/club/{clubId}")
+    public void delete (@PathVariable String clubId) {
+        clubService.remove(clubId);
     }
 
 }
