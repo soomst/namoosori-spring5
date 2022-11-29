a8bdae485e3055cc59e9e0a255be79a1df3452d4
resoom <dogok1703@gmail.com>
2022-11-29 18:19:10 +0900

    @GetMapping, @PathVqriabled 실습

--- travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
+++ travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
@@ -1,9 +1,14 @@
 package io.namoosori.travelclub.web.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.namoosori.travelclub.web.aggregate.club.TravelClub;
 import io.namoosori.travelclub.web.service.ClubService;
 import io.namoosori.travelclub.web.service.sdo.TravelClubCdo;
 
@@ -19,6 +24,16 @@ public class ClubController {
     @PostMapping("/club")
     public String register(@RequestBody TravelClubCdo travelClubCdo) {
         return clubService.registerClub(travelClubCdo);
+    }
+
+    @GetMapping("/club/all")
+    public List<TravelClub> findAll() {
+        return clubService.findAll();
+    }
+
+    @GetMapping("/club/{clubId}")
+    public TravelClub find(@PathVariable String clubId) {
+        return clubService.findClubById(clubId);
     }
 
 }
